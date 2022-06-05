f4acfd245a93bc01878ffd819ad46f807f6a1176
zergrandy <zergrandy@gmail.com>
2022-06-05 22:10:35 +0800

    2022coinsha形象網站

--- js/Element.js
+++
@@ -1,7 +0,0 @@
-//﻿var UsdttwdHandlerUrl = "ws://localhost:58584/WebsocketBack/UsdttwdWebsocketHandler.ashx";
-
-
-//var UsdttwdHandlerUrl = "ws://brickerbtc.servehttp.com/Combine3.4/WebsocketBack/UsdttwdWebsocketHandler.ashx";
-
-
-var UsdttwdHandlerUrl = "wss://www.coinsha.org/WebsocketBack/UsdttwdWebsocketHandler.ashx";
